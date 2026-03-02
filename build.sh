01b112db0cbbb02c09dbf68d47e0558a894adaa6
Kevin Diaz <kevrocks67@gmail.com>
2026-03-02 12:28:32 -0800

    Update keypad and door switch DTS
    I didnt save some linux patches in my
    previous commit to enable evtest in
    my kernel
    
    Changed the keypin matrix to match proper
    KEY_ events for each button
    
    Moved the contact switch back to P9_12
    since P9_42 is used for jtag
    
    Minor change to build script so that it
    defaults to building BBB

--- build.sh
+++ build.sh
@@ -12,10 +12,12 @@ set -e
 cd `dirname $0`
 
 if [[ $1 == "qemu" ]]; then
+    build_type="qemu"
     O_FLAG="O=out_qemu"
     CONFIG_PATH="out_qemu/.config"
     source shared-qemu.sh
 else
+    build_type="bbb"
     O_FLAG=""
     CONFIG_PATH=".config"
     source shared.sh
@@ -23,9 +25,9 @@ fi
 
 # Check if the SPECIFIC folder is configured
 if [ ! -e buildroot/${CONFIG_PATH} ]; then
-    echo "CONFIGURING $1..."
-    make -C buildroot ${O_FLAG} BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} aesd_$1_defconfig
+    echo "CONFIGURING $build_type..."
+    make -C buildroot ${O_FLAG} BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} aesd_${build_type}_defconfig
 fi
 
-echo "BUILDING $1..."
+echo "BUILDING ${build_type}..."
 make -C buildroot ${O_FLAG} BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
